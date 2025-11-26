5c255cc2cf5ab5f08901e27ba01d1af711d9e90b
Pankaj <pk4585634@gmail.com>
2025-11-26 17:44:42 +0530

    Add Student class with attributes and methods
    Implement Student class extending Person with attributes and methods.

---
+++ model/model/Student.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Student extends Person {
+    private int rollNo;
+    private String course;
+    private double marks;
+    private char grade;
+
+    public Student(int rollNo, String name, String email, String course, double marks) {
+        super(name, email);
+        this.rollNo = rollNo;
+        this.course = course;
+        this.marks = marks;
+        calculateGrade();
+    }
+
+    public int getRollNo() { return rollNo; }
+    public String getName() { return name; }
+    public double getMarks() { return marks; }
+
+    private void calculateGrade() {
+        if (marks >= 90) grade = 'A';
+        else if (marks >= 75) grade = 'B';
+        else if (marks >= 50) grade = 'C';
+        else grade = 'D';
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Course: " + course);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("-----------------------------------");
+    }
+
+    public String toFile() {
+        return rollNo + "," + name + "," + email + "," + course + "," + marks;
+    }
+}
